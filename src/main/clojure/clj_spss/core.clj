9b9cb6e17f063c693d91410ffa0d26176fda1987
mikera <mike.r.anderson.13@gmail.com>
2015-11-06 13:26:09 +0800

    Implement basic dataset loading

--- src/main/clojure/clj_spss/core.clj
+++ src/main/clojure/clj_spss/core.clj
@@ -1,2 +1,49 @@
-(ns clj-spss.core)
+(ns clj-spss.core
+  (:require [clojure.core.matrix :as m])
+  (:require [clojure.core.matrix.dataset :as ds])
+  (:require [mikera.cljutils.error :refer [error]])
+  (:import [org.opendatafoundation.data.spss SPSSFile SPSSVariable SPSSStringVariable SPSSNumericVariable])
+  (:import [org.opendatafoundation.data FileFormatInfo])
+  (:import [java.io File]))
 
+(set! *warn-on-reflection* true)
+
+(defn load-spssfile
+  "Loads an SPSS .sav file into an SPSSFile"
+  (^SPSSFile [file]
+    (load-spssfile file nil))
+  (^SPSSFile [file options]
+    (let [^File file (if (string? file) (File. (str file)) file)
+        spssfile (SPSSFile. file)]
+      (set! (.logFlag spssfile) (boolean (:log options))) 
+      (.loadMetadata spssfile)
+      (.loadData spssfile)
+      spssfile)))
+
+(defn converter 
+  "Gets a function that parses an SPSS variable string value" 
+  [^SPSSVariable v]
+  (cond 
+    (instance? SPSSStringVariable v) identity
+    (instance? SPSSNumericVariable v) (fn [s] (and s (read-string s)))
+    :else (error "Unable to recognise SPSS variable type")))
+
+(defn load-spss
+  "Loads a SPSS .sav file into a Clojure data structure"
+  [file]
+  (let [spssfile (load-spssfile file)
+        vcount (.getVariableCount spssfile)
+        rowcount (.getRecordCount spssfile)
+        variables (mapv #(.getVariable spssfile (int %)) (range vcount))
+        variable-names (mapv #(.getName ^SPSSVariable %) variables)
+        ^FileFormatInfo ffi (FileFormatInfo.)
+        columns (mapv 
+                  (fn [^SPSSVariable v]
+                    (let [conv (converter v)]
+                      (mapv 
+                        (fn [i] (conv (.getValueAsString v (int i) ffi)))
+                        (range rowcount))))
+                  variables)]
+    (ds/dataset variable-names
+                (zipmap variable-names 
+                        columns))))
--- src/test/clojure/clj_spss/test_core.clj
+++ src/test/clojure/clj_spss/test_core.clj
@@ -1,4 +1,8 @@
 (ns clj-spss.test-core
   (:use [clj-spss.core])
+  (:use [clojure.core.matrix])
   (:use [clojure.test]))
 
+(deftest test-load
+  (let [ds (load-spss "src/test/resources/org/opendatafoundation/data/spss/DatabaseTest.sav")]
+    (is (= [200 11] (shape ds)))))
