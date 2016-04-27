a1fb463c58ba170521b1fce7164d0316fd6ef403
mikera <mike.r.anderson.13@gmail.com>
2016-04-27 12:54:26 +0800

    Updates for csv conversion

--- src/main/clojure/clj_spss/core.clj
+++ src/main/clojure/clj_spss/core.clj
@@ -1,34 +1,84 @@
 (ns clj-spss.core
   (:require [clojure.core.matrix :as m])
   (:require [clojure.core.matrix.dataset :as ds])
+  (:require [clojure.data.csv :as csv])
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str])
   (:require [mikera.cljutils.error :refer [error]])
-  (:import [org.opendatafoundation.data.spss SPSSFile SPSSVariable SPSSStringVariable SPSSNumericVariable])
+  (:import [org.opendatafoundation.data.spss SPSSFile SPSSUtils SPSSVariable SPSSStringVariable SPSSNumericVariable])
   (:import [org.opendatafoundation.data FileFormatInfo])
-  (:import [java.io File]))
+  (:import [java.io File])
+  (:import [java.net URI]))
 
 (set! *warn-on-reflection* true)
+
+(defn to-file 
+  "Coerces the argument is a java.io.File. Handles existing files, and Strings interpreted as file paths."
+  (^java.io.File [file]
+    (cond 
+      (instance? File file) file
+      (string? file) (File. (str file))
+      (instance? URI file) (File. ^java.net.URI file)
+      :else (error "Can't convert to File: " (class file)))))
 
 (defn load-spssfile
   "Loads an SPSS .sav file into an SPSSFile"
   (^SPSSFile [file]
     (load-spssfile file nil))
   (^SPSSFile [file options]
-    (let [^File file (if (string? file) (File. (str file)) file)
+    (let [file (to-file file)
         spssfile (SPSSFile. file)]
       (set! (.logFlag spssfile) (boolean (:log options))) 
       (.loadMetadata spssfile)
       (.loadData spssfile)
       spssfile)))
 
+(defn write-csv
+  "Loads an SPSS .sav file into an SPSSFile"
+  ([spssdata file]
+    (with-open [out-file (io/writer file)]
+      (csv/write-csv out-file
+                     (concat
+                       [(ds/column-names spssdata)]
+                       (map m/eseq (m/slices spssdata)))))))
+
 (defn converter 
-  "Gets a function that parses an SPSS variable value" 
-  [^SPSSVariable v]
+  "Gets a function that converts an SPSS variable value to a Clojure value.
+
+   Supported types:
+    - Strings
+    - Numbers
+    - Dates 
+
+   Missing values are returned as nil." 
+  [^SPSSVariable v ^FileFormatInfo ffi]
   (cond 
-    (instance? SPSSStringVariable v) identity
+    (instance? SPSSStringVariable v) 
+      (fn [^SPSSVariable v i]
+        (let [s (.getValueAsString v (int i) ffi)]
+          (try
+            (when-not (.isMissingValueCode v s) 
+              s)
+            (catch Throwable t
+              (error "Can't read value: " s " from SPSS variable of type: " (class v) "with format: " format)))))
     (instance? SPSSNumericVariable v) 
       (let [format ^String (.getSPSSFormat v)]
         (cond
-          :else (fn [s] (and s (read-string s)))))
+          (str/includes? (str/upper-case format) "DATE")
+            (fn [^SPSSNumericVariable v i] 
+              (try
+                (let [d (.getValueAsDouble v (int i))]
+                  (when-not (.isMissingValueCode v d)
+                    (SPSSUtils/numericToCalendar d)))
+                (catch Throwable t
+                  (error "Can't read value from SPSS variable of type: " (class v) "with format: " format))))
+          :else (fn [^SPSSNumericVariable v i] 
+                  (let [s (.getValueAsString v (int i) ffi)]
+                    (try
+                      (when-not (empty? s) 
+                        (read-string s))
+                      (catch Throwable t
+                        (error "Can't read value: " s " from SPSS variable of type: " (class v) "with format: " format)))))))
     :else (error "Unable to recognise SPSS variable type")))
 
 (defn load-spss
@@ -44,9 +94,9 @@
           ^FileFormatInfo ffi (FileFormatInfo.)
           columns (mapv 
                     (fn [^SPSSVariable v]
-                      (let [conv (converter v)]
+                      (let [conv (converter v ffi)]
                         (mapv 
-                          (fn [i] (conv (.getValueAsString v (int i) ffi)))
+                          (fn [i] (conv v i))
                           (range rowcount))))
                     variables)]
       (ds/dataset variable-names
--- src/test/clojure/clj_spss/test_core.clj
+++ src/test/clojure/clj_spss/test_core.clj
@@ -1,12 +1,25 @@
 (ns clj-spss.test-core
   (:use [clj-spss.core])
-  (:use [clojure.core.matrix])
+  (:require [clojure.core.matrix :as m])
+  (:require [clojure.string :as str])
+  (:require [clojure.core.matrix])
   (:use [clojure.test]))
 
 (deftest test-load
-  (let [ds (load-spss "src/test/resources/org/opendatafoundation/data/spss/DatabaseTest.sav")]
-    (is (= [200 11] (shape ds)))))
+  (testing "regular file path"
+    (let [ds (load-spss "src/test/resources/org/opendatafoundation/data/spss/DatabaseTest.sav")]
+      (is (= [200 11] (m/shape ds))))))
 
 ;(deftest test-strings
 ;  (let [ds (load-spss "src/test/resources/org/opendatafoundation/data/spss/VeryLongStringVariables.sav")]
 ;    (is (= [200 11] (shape ds)))))
+
+(comment 
+  (doseq [file ["src/test/resources/org/opendatafoundation/data/spss/DatabaseTest.sav"
+                "src/test/resources/org/opendatafoundation/data/spss/StringCategories.sav"
+                "src/test/resources/org/opendatafoundation/data/spss/TestNumber.sav"
+                "src/test/resources/org/opendatafoundation/data/spss/VeryLongStrings.sav"
+                "src/test/resources/org/opendatafoundation/data/spss/VeryLongStringVariables.sav"]]
+    (write-csv (load-spss file) (str/replace file ".sav" ".csv")))
+  
+  )
